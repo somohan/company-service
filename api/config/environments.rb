9d9536cccdbdcd3e1df673af013fbfda243b08cd
Søren Møller Hansen <smh@merk.dk>
2017-09-05 23:05:34 +0200

    Made the application a bit more production ready. Now it works in Docker without any problems.

--- api/config/environments.rb
+++ api/config/environments.rb
@@ -1,10 +1,14 @@
 configure :production, :development do
-  ActiveRecord::Base.establish_connection(
-      :adapter => ENV['DB_ADAPTER'] || 'postgresql',
-      :host     => ENV['DB_HOST'] || 'localhost',
-      :username => ENV['DB_USER'] || 'company_service',
-      :password => ENV['DB_PASS'] || 'pass',
-      :database => ENV['DB_DB_NAME'] || 'company_service',
-      :encoding => 'utf8'
-  )
+  if ENV['DATABASE_URL']
+    ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
+  else
+    ActiveRecord::Base.establish_connection(
+        :adapter => ENV['DB_ADAPTER'] || 'postgresql',
+        :host     => ENV['DB_HOST'] || 'localhost',
+        :username => ENV['DB_USER'] || 'company_service',
+        :password => ENV['DB_PASS'] || 'pass',
+        :database => ENV['DB_DB_NAME'] || 'company_service',
+        :encoding => 'utf8'
+    )
+  end
 end
